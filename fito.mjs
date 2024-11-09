c4efcc8f8271e0f05acf761d377835bb174a0a85
AirStair <AirStair@users.noreply.github.com>
2024-11-09 15:04:35 +0300

    Update fito.mjs

--- fito.mjs
+++ fito.mjs
@@ -1,19 +1,28 @@
 export const ce = new Proxy({}, {
-  get: (...[target, key]) => (raw, ...values) => {
+  get: (...[target, key]) => (observedAttributes = []) => (raw, ...values) => {
     const name = key.split(/(?=[A-Z])/).join('-').toLowerCase();
+    const innerHTML = String.raw({ raw }, ...values);
     class Ce extends HTMLElement {
       constructor() {
         super();
-        const shadow = this.attachShadow({ mode: 'closed' });
-        shadow.innerHTML = String.raw({ raw }, ...values);
-        target.shadow = shadow;
+        this.shadow = this.attachShadow({ mode: 'closed' });
+        this.shadow.innerHTML = innerHTML;
+        target.shadow = this.shadow
+      }
+      static observedAttributes = observedAttributes
+      attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue !== newValue) {
+          this.shadow.innerHTML = this.shadow.innerHTML.replace(`{{${name}}}`, newValue);
+        }
       }
     }
     customElements.define(name, Ce);
-    return (selector, textContent) => {
-      const element = target.shadow.querySelector(selector);
-      element.textContent = textContent;
-      customElements.upgrade(target.shadow);
-    }
+    return new Proxy({}, {
+      set: (...[, key, textContent]) => {
+        const element = target.shadow.querySelector(`slot[name=${key}]`);
+        element.textContent = textContent;
+        customElements.upgrade(target.shadow)
+      }
+    })
   }
 });

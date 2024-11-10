77e904ebf47a440e53195ff7659077ebeefd519a
AirStair <AirStair@users.noreply.github.com>
2024-11-10 22:23:18 +0300

    Update fito.mjs

--- fito.mjs
+++ fito.mjs
@@ -1,24 +1,23 @@
 const ce = new Proxy({}, {
-  get: (...[target, key]) => (observedAttributes = []) => (raw, ...defaultValues) => {
+  get: (...[target, key]) => (observedAttributes = []) => (raw, ...values) => {
     const name = key.split(/(?=[A-Z])/).join('-').toLowerCase();
-    const template = (values = defaultValues) => String.raw({ raw }, ...values);
     class Ce extends HTMLElement {
       constructor() {
         super();
-        this.shadow = this.attachShadow({ mode: 'closed' });
-        this.shadow.innerHTML = template();
-        target.shadow = this.shadow;
+        const shadow = this.attachShadow({ mode: 'closed' });
+        shadow.innerHTML = String.raw({ raw }, ...values);
+        globalThis[key] = (key, value) => {
+          this.setAttribute(key, value);
+        }
       }
       static observedAttributes = observedAttributes
       attributeChangedCallback(name, oldValue, newValue) {
         if (oldValue !== newValue) {
-          this.shadow.innerHTML = template.replaceAll(`{{${name}}}`, newValue);
+          const element = this.querySelector(`[${name}]`);
+          element.setAttribute(name, newValue);
         }
       }
     }
     customElements.define(name, Ce);
-    return () => {
-      customElements.upgrade(target.shadow)
-    }
   }
 });
